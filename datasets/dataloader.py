777c9854f735a9e97c506de6147bde5135f454d7
alexmoed <47517920+alexmoed@users.noreply.github.com>
2025-08-21 15:55:10 +0100

    Update dataloader.py

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -1,3 +1,8 @@
+/// @brief Point cloud processing module
+/// Modified from :
+/// Xiaoyang Wu (2023). Pointcept: A Codebase for Point Cloud Perception Research [online].
+/// [Accessed 2025]. Available from: "https://github.com/Pointcept/Pointcept".
+
 from functools import partial
 import weakref
 import torch
