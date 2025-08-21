581dc9e2cc3121d4217b16c46d749ce9f0b918c6
alexmoed <47517920+alexmoed@users.noreply.github.com>
2025-08-21 16:29:24 +0100

    Update train.py

--- tools/train.py
+++ tools/train.py
@@ -1,9 +1,8 @@
-"""
-Main Training Script
+/// @brief Point cloud processing module
+/// Modified from :-
+/// Xiaoyang Wu (2023). Pointcept: A Codebase for Point Cloud Perception Research [online].
+/// [Accessed 2025]. Available from: "https://github.com/Pointcept/Pointcept".
 
-Author: Xiaoyang Wu (xiaoyang.wu.cs@gmail.com)
-Please cite our work if the code is helpful to you.
-"""
 
 from pointcept.engines.defaults import (
     default_argument_parser,
